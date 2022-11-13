97539d0fd0a6529355135c4e48af9a5ca8ea68c6
Artem Ryblov <ryblovartem@gmail.com>
2022-11-13 21:41:20 +0300

    added plots for feature interactions + small improvements

--- shows_analysis/code/utils/plotting_utils.py
+++ shows_analysis/code/utils/plotting_utils.py
@@ -1,8 +1,9 @@
-from typing import Tuple
+from typing import List, Tuple
 
 import matplotlib.pyplot as plt
 import numpy as np
 import pandas as pd
+import seaborn as sns
 from IPython.display import set_matplotlib_formats
 
 set_matplotlib_formats("retina")
@@ -12,6 +13,7 @@ def plot_per_type(
     dataframe: pd.DataFrame,
     column: str,
     title: str,
+    title_shift: int = 1.1,
     bins: int = 10,
     figsize: Tuple[int] = (8, 4),
 ):
@@ -28,7 +30,8 @@ def plot_per_type(
     ax[1].set_title("Type = series")
 
     f.tight_layout()
-    f.suptitle(title, y=1.1)
+
+    f.suptitle(title, y=title_shift)
     plt.show()
 
 
@@ -36,6 +39,7 @@ def plot_dt_per_type(
     dataframe: pd.DataFrame,
     column: str,
     title: str,
+    title_shift: int = 1.0,
     bins: int = 24,
     figsize: Tuple[int] = (14, 4),
 ):
@@ -55,6 +59,34 @@ def plot_dt_per_type(
     ax[1].set_xticks(range(bins), minor=False)
     ax[1].grid(visible=None)
 
-    f.suptitle(title, y=1.0)
+    f.suptitle(title, y=title_shift)
 
+    plt.show()
+
+
+def plot_catplot(
+    y: str,
+    x: str,
+    hue: str,
+    data: pd.DataFrame,
+    title: str,
+    kind: str = "box",
+    order: List[str] = ["negative", "neutral", "positive"],
+    medianprops: dict = {"color": "red", "lw": 1},
+):
+    if kind == "box":
+        plot = sns.catplot(
+            y=y,
+            x=x,
+            hue=hue,
+            data=data,
+            kind=kind,
+            order=order,
+            medianprops=medianprops,
+        )
+    else:
+        plot = sns.catplot(y=y, x=x, hue=hue, data=data, kind=kind, order=order)
+
+    plot.figure.subplots_adjust(top=0.9)
+    plot.fig.suptitle(title)
     plt.show()
